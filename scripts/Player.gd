860f0507d90f36d3ece3a975cb938bbf7978065f
lorinc <serfozo.lorinc@gmail.com>
2018-09-06 10:27:38 +0200

    Initial commit
    Changes to be committed:
    	new file:   .gitignore
    	new file:   Tileset.tres
    	new file:   default_env.tres
    	new file:   icon.png
    	new file:   icon.png.import
    	new file:   project.godot
    	new file:   scenes/Game.tscn
    	new file:   scenes/TerrainTileset.tscn
    	new file:   scripts/Player.gd
    	new file:   sprites/terrain_tileset.png
    	new file:   sprites/terrain_tileset.png.import

---
+++ scripts/Player.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+export(float) var Acceleration
+export(float) var Max_Speed
+export(float) var Jump_Height
+export(float) var Jump_Time
+
+var velocity = Vector2()
+var gravity = 0
+var jump_velocity = 0
+
+func _ready():
+	gravity = -2*Jump_Height/Jump_Time/Jump_Time
+	jump_velocity = abs(gravity)*Jump_Time
+	print('gravity:', gravity, ' jump velocity:', jump_velocity)
+
+func _physics_process(delta):
+	var input_x = 0
+	if (Input.is_action_pressed('ui_left')): input_x += -1
+	if (Input.is_action_pressed('ui_right')): input_x += 1
+	
+	var desired_vel_x = input_x*Max_Speed
+	var accelerate_dir = sign(desired_vel_x - velocity.x)
+	var new_vel_x = clamp(velocity.x + accelerate_dir*Acceleration*delta*60, -Max_Speed, Max_Speed)
+	
+	# remove scattering
+	if ((velocity.x - desired_vel_x)*(new_vel_x - desired_vel_x) < 0):
+		new_vel_x = desired_vel_x
+	# snappy
+	if (new_vel_x*desired_vel_x < 0):
+		new_vel_x = 0
+
+	# jumping
+	var jump = 0
+	if (Input.is_action_just_pressed('ui_accept') and is_on_floor()):
+		jump = 1
+	var new_vel_y = velocity.y - gravity*delta - jump*jump_velocity
+	velocity = move_and_slide(Vector2(new_vel_x, new_vel_y), Vector2(0, -1))
