43d4c65fa590d065c5fc94419dd0c6c8d97afe82
lorinc <serfozo.lorinc@gmail.com>
2018-09-07 10:54:08 +0200

    Map Generation: autotile pass

--- scripts/TileMapGenerator.gd
+++ scripts/TileMapGenerator.gd
@@ -36,12 +36,59 @@ func do_blocks_pass():
 	# clear player pos
 	set_cell(1, -Ground_Rows, -1)
 
-func _ready():
-	randomize()
+const NONE = 0
+const LEFT = 1
+const RIGHT = 2
+const TOP = 4
+const BOTTOM = 8
+const LEFT_RIGHT = LEFT | RIGHT
+const LEFT_TOP = LEFT | TOP
+const LEFT_BOTTOM = LEFT | BOTTOM
+const RIGHT_TOP = RIGHT | TOP
+const RIGHT_BOTTOM = RIGHT | BOTTOM
+const TOP_BOTTOM = TOP | BOTTOM
+const RIGHT_TOP_BOTTOM = RIGHT | TOP | BOTTOM
+const LEFT_TOP_BOTTOM = LEFT | TOP | BOTTOM
+const LEFT_RIGHT_BOTTOM = LEFT | RIGHT | BOTTOM
+const LEFT_RIGHT_TOP = LEFT | RIGHT | TOP
+const ALL = LEFT | RIGHT | TOP | BOTTOM
+
+func do_autotile_pass():
+	for cell in get_used_cells():
+		var neighbors = 0
+		if get_cell(cell.x - 1, cell.y) != -1: neighbors |= LEFT
+		if get_cell(cell.x + 1, cell.y) != -1: neighbors |= RIGHT
+		if get_cell(cell.x, cell.y - 1) != -1: neighbors |= TOP
+		if get_cell(cell.x, cell.y + 1) != -1: neighbors |= BOTTOM
+		var idx = 0
+		match neighbors:
+			NONE: idx = 0
+			LEFT: idx = 3
+			RIGHT: idx = 1
+			TOP: idx = 8
+			BOTTOM: idx = 2
+			LEFT_RIGHT: idx = 2
+			LEFT_TOP: idx = 9
+			LEFT_BOTTOM: idx = 3
+			RIGHT_TOP: idx = 7
+			RIGHT_BOTTOM: idx = 1
+			TOP_BOTTOM: idx = 5
+			RIGHT_TOP_BOTTOM: idx = 4
+			LEFT_TOP_BOTTOM: idx = 6
+			LEFT_RIGHT_BOTTOM: idx = 2
+			LEFT_RIGHT_TOP: idx = 8
+			ALL: idx = 5
+		set_cell(cell.x, cell.y, idx)
+
+func generate():
 	clear()
 	do_blocks_pass()
+	do_autotile_pass()
+
+func _ready():
+	randomize()
+	generate()
 
 func _input(event):
 	if (get_node('/root/global').DEBUG and event.is_action_pressed('ui_focus_next')):
-		clear()
-		do_blocks_pass()
+		generate()
