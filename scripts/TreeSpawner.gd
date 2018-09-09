5c569dfd51e9a479e4d6aa220fa17f7dfb73c3f7
lorinc <serfozo.lorinc@gmail.com>
2018-09-09 10:47:03 +0200

    plant and save trees in the garden

--- scripts/TreeSpawner.gd
+++ scripts/TreeSpawner.gd
@@ -19,18 +19,32 @@ func generate_trees(spawn_points):
 		points.remove(point_idx)
 		add_child(tree_node)
 
+func load_tree(tree_data):
+	var tree_node = tree_scene.instance()
+	tree_node.position = tree_data['position']
+	tree_node.flip_h = tree_data['flip']
+	tree_node.scale = tree_data['scale']
+	tree_node.modulate = Tree_Modulate
+	add_child(tree_node)
+
 func load_trees():
 	var tree_data_array = get_node('/root/global').trees
 	for tree_data in tree_data_array:
-		var tree_node = tree_scene.instance()
-		tree_node.position = tree_data['position']
-		tree_node.flip_h = tree_data['flip']
-		tree_node.scale = tree_data['scale']
-		tree_node.modulate = Tree_Modulate
-		add_child(tree_node)
+		load_tree(tree_data)
 
 func _on_TileMap_Map_generated(spawn_points, top_pos):
 	if Load_Saved_Trees:
 		load_trees()
 	else:
 		generate_trees(spawn_points)
+
+func _on_PlantTreeButton_pressed():
+	var player = $'../Player'
+	if not player.is_on_floor():
+		return
+	var pos = player.position + Vector2(0, 32)
+	var flip = randi() % 2 == 0
+	var scale = 1 + randi() % 2
+	var data = { 'position': pos, 'flip': flip, 'scale': Vector2(scale, scale) }
+	get_node('/root/global').trees.append(data)
+	load_tree(data)
